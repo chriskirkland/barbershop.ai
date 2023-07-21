12228abee2b807c9ea95db526d50e128f3c8998d
Chris Kirkland <chriskirkland@github.com>
2023-07-21 16:05:25 -0500

    tidy + heart of my heart

--- script/convert.sh
+++ script/convert.sh
@@ -3,6 +3,3 @@ docker run --rm \
   -v $(pwd)/:/data \
   audiveris /audiveris/Audiveris-5.3.1/bin/Audiveris -batch -output /data/output -export \
     $@
-
-    #-option org.audiveris.omr.sheet.BookManager.useCompression=false \
-    #-option org.audiveris.omr.sheet.BookManager.useOpus=true \
